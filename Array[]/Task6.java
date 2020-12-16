0560431abc77d542c31ea5a4cf9f135b85d46996
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2020-12-16 22:10:38 +0300

    Create Task6.java
    Задачи_ Algorithmization

---
+++ Array[]/Task6.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+//6. Задана последовательность N вещественных чисел.
+// Вычислить сумму чисел, порядковые номера которых являются простыми числами.
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter length array A: ");
+        double[] a = new double[console.nextInt()];
+        double sum = 0;
+        System.out.println("array a");
+        for (int i = 0; i <= a.length - 1; i++) {
+            a[i] = Math.random() * 20 - 10;
+            System.out.print(a[i] + " ");
+        }
+        for (int i = 2; i <= a.length - 1; i++) {
+            int count = 0;
+            for (int j = 0; j <= (int) Math.sqrt(i); j++) {
+                if (a[i] % j == 0) {
+                    count++;
+                }
+            } if (count == 0) {
+                sum += a[i];
+            }
+        }
+        System.out.print("\nsum = " + sum);
+    }
+}
