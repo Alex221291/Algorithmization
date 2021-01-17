d3c519b0b69a13fb1e4d1d9680590a3f3e5c4fac
Alex221291 <75179749+Alex221291@users.noreply.github.com>
2021-01-17 21:56:02 +0300

    Задачи_ Algorithmization

---
+++ Methods/Task14.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+//14. Натуральное число, в записи которого n цифр, называется числом Армстронга, если сумма его цифр,
+//возведенная в степень n, равна самому числу. Найти все числа Армстронга от 1 до k. Для решения задачи
+//использовать декомпозицию.
+
+public class Task14 {
+    public static void main(String[] args)  {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter equals k (k => 2):");
+        int k = console.nextInt();
+        numberArmstrong(k);
+    }public static int sumNumber(int num)
+    {
+        char []t = ((String) "" + num).toCharArray();
+        int sNum = 0;
+        for (int i = 0; i < t.length; i++) {
+            sNum += t[i] - '0';
+        }
+        return sNum;
+    }
+    public static int quantNumber(int num)
+    {
+        char []t = ((String) "" + num).toCharArray();
+        return t.length;
+    }
+    public static void numberArmstrong(int k)
+    {
+        System.out.println("Armstrong numbers in the interval [1, " + k + "]");
+        for (int i = 1; i <= k; i++) {
+            if (Math.pow(sumNumber(i), quantNumber(i)) == i) {
+                System.out.print(i + "  ");
+            }
+        }
+    }
+}
